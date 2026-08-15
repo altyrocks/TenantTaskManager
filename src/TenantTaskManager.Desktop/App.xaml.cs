4ec0f1b040859ffa8bb31b94cf62e066e76413b5
Scott Althaus <scott.althaus@gmail.com>
2026-08-15 12:37:59 -0500

    WPF desktop

---
+++ src/TenantTaskManager.Desktop/App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace TenantTaskManager.Desktop;
+
+public partial class App : Application
+{
+}
---
+++ src/TenantTaskManager.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace TenantTaskManager.Desktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
